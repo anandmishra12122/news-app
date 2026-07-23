1f285a6a672b4c928ead0c9953627328c10720dd
anandmishra12122 <anandmishra12122@gmail.com>
2026-07-23 18:49:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,163 @@
+let newsData = {};
+const apikey = "9b6a0144d1000126aa55e33322b05d95";
+
+const homeUrl = `https://gnews.io/api/v4/top-headlines?category=world&lang=en&apikey=${apikey}`;
+
+
+async function getNews(url) {
+    document.getElementById("loading").style.display = "flex";
+    let response = await fetch(url);
+    let data = await response.json();
+    newsData = data;
+    document.getElementById("loading").style.display = "none"
+    console.log(data);
+    breakingNews(newsData);
+    displayNews(newsData);
+}
+getNews(homeUrl);
+
+
+function breakingNews(data) {
+    let ticker = document.querySelector(".ticker");
+    ticker.innerHTML = "";
+
+    for (let i = 0; i < Math.min(2, data.articles.length); i++) {
+        let p = document.createElement("p");
+        p.id = "breaking";
+        p.textContent = data.articles[i].title;
+        ticker.appendChild(p);
+    }
+}
+
+function displayNews(data) {
+    let newsLength = 0;
+    let cardContainer = document.querySelector(".cards-container");
+    document.querySelectorAll(".news-card").forEach(card => card.remove());
+    data.articles.forEach((d) => {
+
+        if (d.image) {
+            let div = document.createElement("div");
+            div.classList.add("news-card");
+
+            let img = document.createElement("img");
+            img.src = d.image;
+            div.appendChild(img);
+
+            let div2 = document.createElement("div");
+            div2.classList.add("card-content");
+
+            let span = document.createElement("span");
+            span.classList.add("news-date");
+            span.textContent = new Date(d.publishedAt).toLocaleString();
+            div2.append(span)
+
+            let h3 = document.createElement("h3");
+            h3.classList.add("news-title");
+            h3.textContent = d.title;
+            div2.append(h3)
+
+            let p = document.createElement("p");
+            p.classList.add("news-description");
+            p.textContent = d.description;
+            div2.append(p);
+
+            let btndiv = document.createElement("div")
+            btndiv.classList.add("card-btn");
+
+            let readbtn = document.createElement("button");
+            let a = document.createElement("a");
+            readbtn.classList.add("read-btn");
+            a.href = d.url;
+            a.textContent = "Read More";
+            readbtn.appendChild(a)
+
+            let sharebtn = document.createElement("button")
+            sharebtn.classList.add("share-btn");
+            let icon = document.createElement("i");
+            icon.className = "fa-solid fa-share";
+            sharebtn.appendChild(icon);
+
+            sharebtn.addEventListener("click", () => {
+                if (navigator.share) {
+                    navigator.share({
+                        title: d.title,
+                        text: d.description,
+                        url: d.url
+                    })
+                        .then(() => console.log("News shared"))
+                        .catch((error) => console.log(error));
+                } else {
+                    navigator.clipboard.writeText(d.url);
+                    alert("News link copied!");
+                }
+            });
+
+            btndiv.appendChild(readbtn);
+            btndiv.appendChild(sharebtn);
+
+
+            div.appendChild(div2);
+            div.appendChild(btndiv);
+            cardContainer.prepend(div);
+            newsLength++;
+        }
+    })
+
+
+    if (newsLength % 4 != 0) {
+        document.querySelector(".no-more-news").style.display = "flex";
+    }
+    else {
+        document.querySelector(".no-more-news").style.display = "none";
+    }
+}
+
+function searchFunction() {
+    let searchbtn = document.getElementById("searchbtn");
+    let searchinput = document.getElementById("searchinput");
+
+    searchbtn.addEventListener("click", () => {
+        let searchValue = searchinput.value.trim();
+
+        if (searchValue) {
+            const searchUrl = `https://gnews.io/api/v4/search?q=${encodeURIComponent(searchValue)}&lang=en&apikey=${apikey}`;
+            getNews(searchUrl);
+            searchinput.value = "";
+        }
+    })
+}
+searchFunction();
+
+
+const categories = document.querySelectorAll("#alllist li");
+
+categories.forEach((category) => {
+    category.addEventListener("click", () => {
+
+        let categoryName = category.innerText.toLowerCase();
+
+        const url = `https://gnews.io/api/v4/top-headlines?category=${categoryName}&lang=en&apikey=${apikey}`;
+
+
+        getNews(url);
+
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
